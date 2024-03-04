67eff3de2dc58d22cc80a21a48c89251f221a1a2
lukasaey <77332021+lukasaey@users.noreply.github.com>
2024-03-04 15:55:42 +0100

    Format Primjeri uporabe.c

--- Primjeri uporabe.c
+++ Primjeri uporabe.c
@@ -1,65 +1,69 @@
-//1.Kratka sintaksa za jednostavne uvjete:
+// 1.Kratka sintaksa za jednostavne uvjete:
 #include <stdio.h>
 
 int main() {
-char *stanje = "otvoreno";
-char *poruka = (stanje == "otvoreno") ? "Dobrodošli!" : "Trenutno zatvoreno.";
+    char *stanje = "otvoreno";
+    char *poruka =
+        (stanje == "otvoreno") ? "Dobrodošli!" : "Trenutno zatvoreno.";
 
-printf("%s\n", poruka);  // Output: Dobrodošli!
+    printf("%s\n", poruka);  // Output: Dobrodošli!
 
-return 0;
+    return 0;
 }
 
-//2.Provjeravanje više uvjeta koristeći uvjetni operator:
+// 2.Provjeravanje više uvjeta koristeći uvjetni operator:
 #include <stdio.h>
 
 int main() {
-int broj = 75;
+    int broj = 75;
 
-(broj > 100) ? printf("Broj je veći od 100.\n") : (broj > 50) ? printf("Broj je veći od 50, ali manji ili jednak 100.\n") : printf("Broj je manji ili jednak 50.\n");
+    (broj > 100)  ? printf("Broj je veći od 100.\n")
+    : (broj > 50) ? printf("Broj je veći od 50, ali manji ili jednak 100.\n")
+                  : printf("Broj je manji ili jednak 50.\n");
 
-// Output: Broj je veći od 50, ali manji ili jednak 100.
+    // Output: Broj je veći od 50, ali manji ili jednak 100.
 
-return 0;
+    return 0;
 }
 
-//3.Uvjetno izvršavanje blokova koda:
+// 3.Uvjetno izvršavanje blokova koda:
 #include <stdio.h>
 
 int main() {
-int broj = 7;
+    int broj = 7;
 
-(broj % 2 == 0) ? printf("Broj je paran.\n") : printf("Broj je neparan.\n");
+    (broj % 2 == 0) ? printf("Broj je paran.\n") : printf("Broj je neparan.\n");
 
-// Output: Broj je neparan.
+    // Output: Broj je neparan.
 
-return 0;
+    return 0;
 }
 
-//4.Ispisivanje poruke na temelju uvjeta:
+// 4.Ispisivanje poruke na temelju uvjeta:
 #include <stdio.h>
 
 int main() {
-int broj = 15;
+    int broj = 15;
 
-(broj > 10) ? printf("Broj je veći od 10.\n") : printf("Broj je manji ili jednak 10.\n");
+    (broj > 10) ? printf("Broj je veći od 10.\n")
+                : printf("Broj je manji ili jednak 10.\n");
 
-// Output: Broj je veći od 10.
+    // Output: Broj je veći od 10.
 
-return 0;
+    return 0;
 }
 
-//5.Dodjela vrijednosti varijabli na temelju uvjeta:
+// 5.Dodjela vrijednosti varijabli na temelju uvjeta:
 #include <stdio.h>
 
 int main() {
-int x = 10;
-int y = 5;
+    int x = 10;
+    int y = 5;
 
-// Ako je x veći od y, dodijeli mu vrijednost 100, inače dodijeli 0
-int rezultat = (x > y) ? 100 : 0;
+    // Ako je x veći od y, dodijeli mu vrijednost 100, inače dodijeli 0
+    int rezultat = (x > y) ? 100 : 0;
 
-printf("%d\n", rezultat);  // Output: 100
+    printf("%d\n", rezultat);  // Output: 100
 
-return 0;
+    return 0;
 }
